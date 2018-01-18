a81dec0c23c48348ca085df3f57cdc8d634e2861
pizhanska <pizhanska29@gmail.com>
2018-01-18 23:55:47 +0200

    add events on user's buttons

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var csrf = require('csurf');
-var passport = require('passport');
+
 var Book = require('../models/book')
 
-var csrfProtection = csrf();
-router.use(csrfProtection);
-
-router.get('/', function(req, res, next) {
-  Book.find(function (err, docs) {
-    var bookChunks = [];
-    var chunkSize = 2;
-    for(var i=0; i < docs.length; i+=chunkSize){
-      bookChunks.push(docs.slice(i, i+chunkSize));
-    }
-    res.render('shop/index', { title: 'Book Store', books: bookChunks });
-  });
+router.get('/', function (req, res, next) {
+    Book.find(function (err, docs) {
+        var bookChunks = [];
+        var chunkSize = 2;
+        for (var i = 0; i < docs.length; i += chunkSize) {
+            bookChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('shop/index', {title: 'Book Store', books: bookChunks});
+    });
 });
-
-router.get('/user/signup',function(req, res, next) {
-    var messages = req.flash('error');
-    res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
-});
-
-router.post('/user/signup', passport.authenticate('local.signup', {
-    successRedirect: '/user/profile',
-    failureRedirect: '/user/signup',
-    failureFlash: true
-}) );
-
-router.get('/user/profile', function (req, res, next) {
-   res.render('user/profile');
-});
-
 module.exports = router;
